12013d3e04944fe3b3aef203d8fa61a87a822f72
Nyttep <duboispaul85170@hotmail.fr>
2024-07-20 21:44:19 +0200

    ex04

---
+++ day00/ex04/whatis.py
@@ -0,0 +1,14 @@
+import sys as sys
+
+if len(sys.argv) > 2:
+	sys.exit("AssertionError: more than one argument is provided")
+if len(sys.argv) < 2:
+	sys.exit(0)
+
+try:
+	if int(sys.argv[1]) % 2 == 0:
+		print("I'm Even.")
+	else:
+		print("I'm Odd.")
+except ValueError:
+	sys.exit("AssertionError: argument is not an integer")
