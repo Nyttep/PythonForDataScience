5c6b6a1858555cb9f12c6d6e962668dbc0e89aaf
Nyttep <duboispaul85170@hotmail.fr>
2024-07-20 22:16:54 +0200

    formatted

--- day00/ex04/whatis.py
+++ day00/ex04/whatis.py
@@ -1,14 +1,14 @@
 import sys as sys
 
 if len(sys.argv) > 2:
-	sys.exit("AssertionError: more than one argument is provided")
+    sys.exit("AssertionError: more than one argument is provided")
 if len(sys.argv) < 2:
-	sys.exit(0)
+    sys.exit(0)
 
 try:
-	if int(sys.argv[1]) % 2 == 0:
-		print("I'm Even.")
-	else:
-		print("I'm Odd.")
+    if int(sys.argv[1]) % 2 == 0:
+        print("I'm Even.")
+    else:
+        print("I'm Odd.")
 except ValueError:
-	sys.exit("AssertionError: argument is not an integer")
+    sys.exit("AssertionError: argument is not an integer")
