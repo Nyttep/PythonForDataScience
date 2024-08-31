7c0c61dcd1b129c49414285722beb867b9138cb1
Nyttep <duboispaul85170@hotmail.fr>
2024-08-31 23:57:45 +0200

    normed

--- day00/ex08/Loading.py
+++ day00/ex08/Loading.py
@@ -1,10 +1,9 @@
 from os import get_terminal_size
-import sys
+
 
 def ft_tqdm(lst: range) -> None:
     term_width = get_terminal_size().columns
     white = "\033[47m \033[m"
-    # white = "="
     black = " "
     total = len(lst)
     bar_length = term_width - (34 + len(str(total) * 2))
@@ -12,7 +11,8 @@ def ft_tqdm(lst: range) -> None:
     for elem in lst:
         i += 1
         bar_progress = (int)((i / total) * bar_length)
-        white_bar = white * bar_progress
-        black_bar = black * abs(bar_progress - bar_length)
-        print("{:>4.0%}|{}{}| {}/{}".format(i/total, white_bar, black_bar, i, total), end="\r", flush=True)
+        w_bar = white * bar_progress
+        b_bar = black * abs(bar_progress - bar_length)
+        txt = "{:>4.0%}|{}{}| {}/{}".format(i/total, w_bar, b_bar, i, total)
+        print(txt, end="\r", flush=True)
         yield elem
