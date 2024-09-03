6c75149be4c9b9d4de3862cfc29e52d75a66963b
Nyttep <duboispaul85170@hotmail.fr>
2024-09-03 22:56:53 +0200

    normed

--- day01-Array/ex02/load_image.py
+++ day01-Array/ex02/load_image.py
@@ -2,17 +2,17 @@ from PIL import Image
 import numpy as np
 
 
-def ft_load(path: str)-> np.array:
+def ft_load(path: str) -> np.array:
     '''ft_load(path: str)-> np.array
-    
-Load an image from the path, 
+
+Load an image from the path,
 print its format and its pixel content in RGB format
 return the image as a numpy array
 If the image can't be loaded return None and prints the error'''
     try:
         img = Image.open(path)
     except IOError:
-        if path == None:
+        if path is None:
             print("Error: No path provided")
         elif path == "":
             print("Error: Empty path provided")
