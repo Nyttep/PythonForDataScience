90f37ccef7ca788d523dcf627b53e327e965cf84
Nyttep <duboispaul85170@hotmail.fr>
2024-09-07 18:43:21 +0200

    added __doc__ and error handling

--- day02-DataTable/ex00/load_csv.py
+++ day02-DataTable/ex00/load_csv.py
@@ -2,6 +2,18 @@ import pandas as pd
 
 
 def load(path: str) -> pd.DataFrame:
-    csv = pd.read_csv(path)
+    '''load(path: str) -> pd.DataFrame
+
+Load a csv file from the given path and return a pandas DataFrame.
+Return None if the file is not found or if any error occured.
+'''
+    try:
+        csv = pd.read_csv(path)
+    except FileNotFoundError:
+        print(f"File not found: {path}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
     print(f"Loading dataset of dimensions {csv.shape}")
     return csv
