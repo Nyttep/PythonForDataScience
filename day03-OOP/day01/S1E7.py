d791acf651e8feb35f032a8f9901a34b21ffcc3b
Nyttep <duboispaul85170@hotmail.fr>
2024-09-20 20:49:17 +0200

    starting ex01 I absolutely HATE the look of docstrings (but love what it does)

---
+++ day03-OOP/day01/S1E7.py
@@ -0,0 +1,47 @@
+from S1E9 import Character
+
+class Baratheon(Character):
+    """Class for the Baratheon family, herits from the Character class
+
+Attributes:
+    first_name (str): The first name of the character
+    is_alive (bool): The status of the character
+
+Methods:
+    die: Kill the character"""
+
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """Constructor for the Baratheon class
+
+Args:
+    first_name (str): The first name of the character
+Optional args:
+    is_alive (bool): Define if the character is alive, default is True"""
+        super().__init__(first_name, is_alive)
+
+    def die(self):
+        """Kill the character"""
+        super().die()
+
+class Lannister(Character):
+    """Class for the Lannister family, herits from the Character class
+
+Attributes:
+    first_name (str): The first name of the character
+    is_alive (bool): The status of the character
+
+Methods:
+    die: Kill the character"""
+
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """Constructor for the Lannister class
+
+Args:
+    first_name (str): The first name of the character
+Optional args:
+    is_alive (bool): Define if the character is alive, default is True"""
+        super().__init__(first_name, is_alive)
+
+    def die(self):
+        """Kill the character"""
+        super().die()
---
+++ day03-OOP/day01/S1E9.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+
+
+class Character(ABC):
+    """Abstract class for a character
+
+Attributes:
+    first_name (str): The first name of the character
+    is_alive (bool): The status of the character
+
+Methods:
+    die: Kill the character"""
+
+    @abstractmethod
+    def __init__(self, first_name: str, is_alive: bool = True):
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+    @abstractmethod
+    def die(self):
+        self.is_alive = False
+
+
+class Stark(Character):
+    """Class for the Stark family, herits from the Character class
+
+Attributes:
+    first_name (str): The first name of the character
+    is_alive (bool): The status of the character
+
+Methods:
+    die: Kill the character"""
+
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """Constructor for the Stark class
+
+Args:
+    first_name (str): The first name of the character
+Optional args:
+    is_alive (bool): Define if the character is alive, default is True"""
+        super().__init__(first_name, is_alive)
+
+    def die(self):
+        """Kill the character"""
+        super().die()
