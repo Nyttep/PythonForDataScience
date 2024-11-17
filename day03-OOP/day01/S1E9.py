2aa95490d97ab25436d170c8e6fe9949849a9ad3
Nyttep <duboispaul85170@hotmail.fr>
2024-11-17 18:51:36 +0100

    used copilot to reformat S1E9.py docstring wtf its so cool (NOTE TO SELF DONT GET LAZY WRITE MY OWN CODE i should use it only for things like that)

--- day03-OOP/day01/S1E9.py
+++ day03-OOP/day01/S1E9.py
@@ -2,14 +2,16 @@ from abc import ABC, abstractmethod
 
 
 class Character(ABC):
-    """Abstract class for a character
+    """
+    Abstract class for a character.
 
-Attributes:
-    first_name (str): The first name of the character
-    is_alive (bool): The status of the character
+    Attributes:
+        first_name (str): The first name of the character.
+        is_alive (bool): The status of the character.
 
-Methods:
-    die: Kill the character"""
+    Methods:
+        die: Kill the character.
+    """
 
     @abstractmethod
     def __init__(self, first_name: str, is_alive: bool = True):
@@ -22,24 +24,29 @@ Methods:
 
 
 class Stark(Character):
-    """Class for the Stark family, herits from the Character class
+    """
+    Class for the Stark family, inherits from the Character class.
 
-Attributes:
-    first_name (str): The first name of the character
-    is_alive (bool): The status of the character
+    Attributes:
+        first_name (str): The first name of the character.
+        is_alive (bool): The status of the character.
 
-Methods:
-    die: Kill the character"""
+    Methods:
+        die: Kill the character.
+    """
 
     def __init__(self, first_name: str, is_alive: bool = True):
-        """Constructor for the Stark class
+        """
+        Constructor for the Stark class.
 
-Args:
-    first_name (str): The first name of the character
-Optional args:
-    is_alive (bool): Define if the character is alive, default is True"""
+        Args:
+            first_name (str): The first name of the character.
+            is_alive (bool, optional): Define if the character is alive, default is True.
+        """
         super().__init__(first_name, is_alive)
 
     def die(self):
-        """Kill the character"""
+        """
+        Kill the character.
+        """
         self.is_alive = False
