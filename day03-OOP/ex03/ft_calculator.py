1eaeb22ae235efbae587be13781ee570f28abb00
Nyttep <duboispaul85170@hotmail.fr>
2024-11-28 17:42:25 +0100

    add calculator class with basic arithmetic operations on vectors

---
+++ day03-OOP/ex03/ft_calculator.py
@@ -0,0 +1,21 @@
+class calculator:
+    """
+    """
+
+    def __init__(self, vect: list) -> None:
+        self.vect = vect
+
+    def __add__(self, rhs) -> None:
+        print([x + rhs for x in self.vect])
+
+    def __mul__(self, rhs) -> None:
+        print([x * rhs for x in self.vect])
+
+    def __sub__(self, rhs) -> None:
+        print([x - rhs for x in self.vect])
+
+    def __truediv__(self, rhs) -> None:
+        if (rhs == 0):
+            print("Error: division by zero is impossible")
+        else:
+            print([x / rhs for x in self.vect])
