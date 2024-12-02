532f080088eba2d8d723698b1bfc891fffb12b40
Nyttep <duboispaul85170@hotmail.fr>
2024-12-02 16:29:44 +0100

    refactor statistics functions to return values instead of printing so i print in main

--- day04-Dod/ex00/statistics.py
+++ day04-Dod/ex00/statistics.py
@@ -2,42 +2,41 @@ from typing import Any
 from numbers import Number
 
 
-def ft_mean(args: list[Number]) -> None:
+def ft_mean(args: list[Number]) -> float:
     mean = sum(args) / len(args)
-    print(f"mean : {mean}")
+    return mean
 
 
-def ft_median(args: list[Number]) -> None:
+def ft_median(args: list[Number]) -> float:
     lenght = len(args)
     sort = sorted(args)
     pos = lenght // 2
-    if pos == -1 :
+    if pos == -1:
         pos = 0
     if lenght % 2 == 1:
         median = sort[pos]
     else:
-        median = (sort[pos - 1] +  sort[pos]) / 2
-    print(f"median : {median}")
+        median = (sort[pos - 1] + sort[pos]) / 2
+    return median
 
 
-def ft_quartile(args: list[Number]) -> None:
+def ft_quartile(args: list[Number]) -> float:
     lenght: int = len(args)
     sort: list[Number] = sorted(args)
     pos1: int = ((lenght + 3) // 4) - 1
     pos3: int = (((lenght * 3) + 1) // 4) - 1
     quart: list[float] = [0, 0]
-    if pos1 < 0 :
+    if pos1 < 0:
         pos1 = 0
-    if pos1 == ((lenght + 3) // 4) - 1 :
+    if pos1 == ((lenght + 3) // 4) - 1:
         quart[0] = sort[pos1]
     else:
         quart[0] = (sort[pos1] + sort[pos1 + 1]) / 2
-    if pos3 == (((lenght * 3) + 1) // 4) - 1 :
+    if pos3 == (((lenght * 3) + 1) // 4) - 1:
         quart[1] = sort[pos3]
     else:
         quart[1] = (sort[pos3] + sort[pos3 + 1]) / 2
-    print(f"quartile : {quart}")
-    pass
+    return quart
 
 
 def ft_statistics(*args: Any, **kwargs: Any) -> None:
@@ -46,7 +45,7 @@ def ft_statistics(*args: Any, **kwargs: Any) -> None:
         "median": ft_median,
         "quartile": ft_quartile,
     }
-    
+
     myargs = [x for x in args if type(x) != str]
 
     if len(myargs) == 0:
@@ -54,6 +53,7 @@ def ft_statistics(*args: Any, **kwargs: Any) -> None:
 
     for x in kwargs.values():
         try:
-            functions[x](myargs)
+            res: float = functions[x](myargs)
+            print(f"{x} : {res}")
         except KeyError:
             pass
